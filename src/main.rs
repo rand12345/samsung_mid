779c43b6532c2d16592db6c0f17366f4bf516faa
CoD <caravanofdeath@icloud.com>
2023-02-17 19:33:42 +0000

    playing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use tokio::time::{sleep, Duration};
+
+async fn delay_ms(dur: u64) {
+    sleep(Duration::from_millis(dur)).await;
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     use tokio_serial::SerialStream;
@@ -11,12 +17,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = SerialStream::open(&builder).unwrap();
     /*
     instrument.write_registers(7005,[0x42E9, 0x42F1, 0x4067, 0x8204])
-     */
+    */
     let mut ctx = rtu::connect_slave(port, slave).await?;
+    delay_ms(500).await;
+    println!("Sending init - enabling regs");
+    ctx.write_multiple_registers(7005, &[0x42E9, 0x42F1, 0x4067, 0x8204])
+        .await?;
     println!("Reading a sensor value");
-    // let _a = ctx
-    //     .write_multiple_registers(7005, &[0x42E9, 0x42F1, 0x4067, 0x8204])
-    //     .await?;
+    delay_ms(500).await;
     let rsp = ctx
         .read_holding_registers(ReadReg::FlowRate as u16, 2)
         .await?;
@@ -29,4 +37,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 enum ReadReg {
     FlowRate = 87,
     ThreeWay = 89,
+    DhwTemp = 75,
+    ReturnTemp = 65,
+    FlowTemp = 66,
+    TargetFlowTemp = 68,
+    DhwStatus = 72,
+    TargetDwhTemp = 74,
+    ChStatus = 52,
+    IndoorTemp = 59,
+    TargetIndoorTemp = 58,
 }
