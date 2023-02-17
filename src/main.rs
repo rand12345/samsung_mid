da94686e8a9d2a56fa16b549e7a84003219af4ba
CoD <caravanofdeath@icloud.com>
2023-02-17 20:26:13 +0000

    test no delay

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         TargetIndoorTemp,
     ] {
         device.read(val).await?;
-        delay_ms(10).await;
+        // delay_ms(10).await;
     }
 
     println!("data: {:#?}", device.pump);
