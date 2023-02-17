6eedb7e6d9ddf1fdac7128a00f938ff0bdacffc6
CoD <caravanofdeath@icloud.com>
2023-02-17 23:27:55 +0000

    Tidy up logic

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@ use tokio::time::{sleep, Duration};
 use tokio_modbus::prelude::Writer;
 use tokio_modbus::{client::Context, prelude::Reader};
 
-async fn delay_ms(dur: u64) {
-    sleep(Duration::from_millis(dur)).await;
-}
+const COMMAND_DELAY: Duration = Duration::from_millis(100);
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     use tokio_serial::SerialStream;
@@ -19,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let builder = tokio_serial::new(tty_path, 9600)
         .parity(tokio_serial::Parity::Even)
-        .timeout(Duration::from_millis(100));
+        .timeout(COMMAND_DELAY);
     let port = SerialStream::open(&builder).unwrap();
     let ctx = rtu::connect_slave(port, slave).await?;
 
@@ -45,11 +44,13 @@ async fn keyboard(tx: Sender<Order>) -> Result<(), MyError> {
     let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
     let mut buffer = Vec::new();
     loop {
-        println!("'r' to read, 'u' +ch, 'd' -ch, 'p' +dwh, 'l' -dhw, 'c' ch mode, 'w' dhw mode");
+        println!(
+            "'r' to read temps, 'u' +ch, 'd' -ch, 'p' +dwh, 'l' -dhw, 'c' ch mode, 'w' dhw mode"
+        );
         let _fut = reader.read_until(b'\n', &mut buffer).await;
         println!("Input was: {buffer:?}",);
         if let Err(e) = match buffer[0] {
-            b'r' => tx.send(Order::Get(Request::DhwTemp)).await,
+            b'r' => tx.send(Order::Get(Request::Temps)).await,
             b'u' => tx.send(Order::Set(Instruction::ChUp)).await,
             b'd' => tx.send(Order::Set(Instruction::ChDown)).await,
             b'p' => tx.send(Order::Set(Instruction::DhwUp)).await,
@@ -83,9 +84,9 @@ struct Pump {
     return_temp: i16,
     flow_temp: i16,
     target_flow_temp: i16,
-    dhw_status: bool,
+    // dhw_status: bool,
     target_dwh_temp: i16,
-    ch_status: bool,
+    // ch_status: bool,
     indoor_temp: i16,
     target_indoor_temp: i16,
     set_target_indoor_temp: i16,
@@ -147,13 +148,24 @@ impl Device {
             self.readall().await?;
             println!("Read vals");
 
-            match rx.try_recv().unwrap() {
-                // poll this
+            let recv = match rx.recv().await {
+                Some(v) => v,
+                None => continue,
+            };
+
+            match recv {
                 Order::Get(request) => {
                     println!("Received request {request:?}");
                     match request {
-                        Request::DhwTemp => self.read(DhwTemp).await?,
-                        Request::ChTemp => self.read(FlowTemp).await?,
+                        Request::Temps => {
+                            self.read(DhwTemp).await?;
+                            self.read(TargetDwhTemp).await?;
+                            self.read(FlowTemp).await?;
+                            self.read(TargetFlowTemp).await?;
+                            self.read(IndoorTemp).await?;
+                            self.read(TargetIndoorTemp).await?;
+                            self.read(ReturnTemp).await?;
+                        }
                     }
                 }
                 Order::Set(command) => match command {
@@ -192,13 +204,15 @@ impl Device {
                     }
                     Instruction::Dwh => {
                         println!("Command process: {command:?}");
+                        self.write(WriteReg::DhwMode, 1).await?;
+                        self.write(WriteReg::ChMode, 0).await?; // not sure if needed
                         self.pump.set_mode(Mode::Dhw);
-                        self.write(WriteReg::DhwMode, 1).await?
                     }
                     Instruction::Ch => {
                         println!("Command process: {command:?}");
+                        self.write(WriteReg::ChMode, 1).await?;
+                        self.write(WriteReg::DhwMode, 0).await?; // not sure if needed
                         self.pump.set_mode(Mode::Ch);
-                        self.write(WriteReg::ChMode, 1).await?
                     }
                 },
             }
@@ -226,16 +240,16 @@ impl Device {
     }
 
     async fn write(&mut self, reg: WriteReg, val: u16) -> Result<(), Box<dyn std::error::Error>> {
-        delay_ms(100).await;
+        sleep(COMMAND_DELAY).await;
         self.bus.write_single_register(reg as u16, val).await?;
         println!("Wrote {val} to {reg:?}");
         Ok(())
     }
     async fn read(&mut self, val: ReadReg) -> Result<(), Box<dyn std::error::Error>> {
-        delay_ms(100).await;
-        print!("Reading a sensor value {val:?}... ");
+        sleep(COMMAND_DELAY).await;
+        print!("Reading a sensor value {val:?}: ");
         let rsp = self.bus.read_holding_registers(val as u16, 1).await?;
-        // println!("Sensor value is: {rsp:?} for {val:?}");
+        println!("{rsp:?}");
         self.decode(rsp[0], val);
         Ok(())
     }
@@ -257,13 +271,15 @@ impl Device {
                 self.pump.target_flow_temp = rsp as i16;
             }
             ReadReg::DhwStatus => {
-                self.pump.dhw_status = rsp == 1;
+                // self.pump.dhw_status = rsp == 1;
+                self.pump.mode = Mode::Dhw;
             }
             ReadReg::TargetDwhTemp => {
                 self.pump.target_dwh_temp = rsp as i16;
             }
             ReadReg::ChStatus => {
-                self.pump.ch_status = rsp == 1;
+                // self.pump.ch_status = rsp == 1;
+                self.pump.mode = Mode::Ch;
             }
             ReadReg::IndoorTemp => {
                 self.pump.indoor_temp = rsp as i16;
@@ -330,8 +346,7 @@ enum Instruction {
 #[allow(dead_code)]
 #[derive(Debug)]
 enum Request {
-    DhwTemp,
-    ChTemp,
+    Temps,
 }
 
 #[derive(Debug)]
